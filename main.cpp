b68d04f11fedeeebc6bd2c8445202d6059195cb1
Rene Gomez <renegmz21@gmail.com>
2019-12-15 15:05:55 -0400

    Commit inicial

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: renegomez
+ *
+ * Created on 15 de diciembre de 2019, 03:02 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+

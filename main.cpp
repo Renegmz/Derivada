8c44d2218b97f440bb8494cd003775ec12fd8893
Rene Gomez <renegmz21@gmail.com>
2019-12-15 17:24:31 -0400

    1er commmit: Ejercicio terminado

--- main.cpp
+++ main.cpp
@@ -6,14 +6,33 @@
  */
 
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+int main() {
+    double h, metodo1,error1, metodo2, error2;
+    
+    /*Valor base para el calculo del error(absoluto)*/
+    double val_base = (1.0)*exp(-pow(1.0,2.0));
+     
+    
+    cout << fixed << setprecision(12) << endl;
+    for(int j = 0; j < 65; j++)
+    {
+       h = 1.0/pow(2.0,j);
+       
+       /* (f(x0 + h) - f(x0)) / h*/
+       metodo1 = ( ((1.0 + h)*exp(-pow(1.0 + h,2.0))) - ((1.0)*exp(-pow(1.0,2.0))) ) / h;
+       error1 = abs(val_base - metodo1); //Error absoluto: |p - p*|
+       
+       /* (f(x0 + h) - f(x0 - h)) / 2h*/
+       metodo2 = ( ((1.0 + h)*exp(-pow(1.0 + h,2.0))) - ((1.0 - h)*exp(-pow(1.0 - h,2.0))) ) / 2*h;
+       error2 = abs(val_base - metodo2); //Error absoluto: |p - p*|
+       
+       cout << h << " " << metodo1 << " " << error1 << " " << metodo2 << " " << error2 <<endl; 
+    }
     return 0;
 }
 
